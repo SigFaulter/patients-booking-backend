64c2a3ac1336c4c896924f312570dc26c81e5af3
SigFaulter <SigFaulter@github.com>
2023-03-22 16:17:07 +0000

    remove unused features

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,12 +49,10 @@ class UserController extends Controller
         // TODO delete related records in patients table
         // $user->delete();
 
+        // TODO redirect user to logout
         return response()->json([
             'success' => true,
             'message' => 'User deleted successfully'
         ], 200)->withCookie(cookie('token', '', 1, null, null, false, true));
-
-        redirect('/home/dashboard');
     }
 }
-?>
