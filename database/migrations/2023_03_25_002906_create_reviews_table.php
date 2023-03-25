111055f41f6b850f0573c7e5adecd7d09312e82f
SigFaulter <SigFaulter@github.com>
2023-03-25 03:40:18 +0000

    add reviews table

---
+++ database/migrations/2023_03_25_002906_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('patient_id')->unsigned()->index()->nullable();
+            $table->foreign('patient_id')->references('patient_id')->on('patients')->onDelete('cascade');
+            $table->bigInteger('doctor_id')->unsigned()->index()->nullable();
+            $table->foreign('doctor_id')->references('doctor_id')->on('doctors')->onDelete('cascade');
+            $table->unsignedInteger('ratings')->nullable();
+            $table->longText('reviews')->nullable();
+            $table->string('reviewed_by');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
