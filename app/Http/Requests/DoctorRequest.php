669691c9100b05f5a3fa57834ab8745042cf5b2f
SigFaulter <SigFaulter@github.com>
2023-04-04 20:47:29 +0000

    code cleanup

--- app/Http/Requests/DoctorRequest.php
+++ app/Http/Requests/DoctorRequest.php
@@ -13,7 +13,7 @@ class DoctorRequest extends BaseRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/RegisterDoctorRequest.php
+++ app/Http/Requests/RegisterDoctorRequest.php
@@ -12,7 +12,7 @@ class RegisterDoctorRequest extends BaseRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
